310a9251c6ef1838c7d56c31f2b5e64057da5f09
Twonki <Leonhard.Applis@Protonmail.com>
2019-02-03 22:18:39 +0100

    Precedence is now on the fly - i removed a lot of touples

--- Parser.hs
+++ Parser.hs
@@ -29,6 +29,59 @@ tokenToOperator t =
         else Left (fromJust mop)
     where operators = [("(",Lbr),(")",Rbr),("Ln",LnE),("**",StarStar),("*",Star),("/",DivSlash),("+",Plus),("-",Minus)]
 
+
+detectVarsAndNumbers :: [Either Operator Token] -> [Either Operator Term]
+detectVarsAndNumbers [] = []
+detectVarsAndNumbers ((Left e):es) = (Left e) : detectVarsAndNumbers es 
+detectVarsAndNumbers ((Right e):es) = (Right (tokenToTerm e)) : (detectVarsAndNumbers es)
+
+tokenToTerm :: Token -> Term
+tokenToTerm [] = Numb 0
+tokenToTerm t@(s:ss) = if (not (elem s ['a'..'z'])) --s is a number, or atleast not a char
+                        then Numb (read t)
+                        else Var t --TODO: Catch Constants here
+
+-- For testing purposes and to show general flow
+initialParse :: String -> [Either Operator Term]
+initialParse s = detectVarsAndNumbers (map tokenToOperator (tokenize s))
+
+termify:: [Either Operator Term] -> Term 
+termify [] = undefined
+-- I've reached a core-term, such as a variable or a Number
+termify [Right t] = t
+-- I've got Operators
+termify t = let 
+                fst = firstOperator t
+                (lhs,Left op,rhs) = splitByFirst t fst 
+                lnb' = last lhs 
+                rnb'= head rhs 
+                lleft = take (length lhs -1) lhs 
+                rleft = drop 1 rhs 
+            in 
+                -- I check if i reached a term
+                if (isRight fst)
+                then termify [fst]
+                else
+                    -- I check if my neigboors are Terms?
+                    if((isRight lnb') && (isRight rnb'))
+                    then 
+                        let 
+                            lcasted = fromRight (Numb 0) lnb'
+                            rcasted = fromRight (Numb 0) rnb'
+                        in
+                            case op of 
+                                -- Unary Operator
+                                LnE -> termify (lhs ++ Right (Ln rcasted) : rleft)
+                                -- Binary Operator
+                                StarStar -> termify (lleft ++ Right (Pow lcasted rcasted) : rleft)
+                                Plus -> termify (lleft ++ Right (Add lcasted rcasted) : rleft)
+                                Minus -> termify (lleft ++ Right (Sub lcasted rcasted) : rleft)
+                                Star -> termify (lleft ++ Right (Mul lcasted rcasted) : rleft)
+                                DivSlash -> termify (lleft ++ Right (Div lcasted rcasted) : rleft)
+                                -- Any other stuff ?
+                                otherwise -> undefined
+                    else undefined
+
 tokenize :: String -> [Token]
 tokenize s = words s 
 
@@ -49,81 +102,32 @@ precedence (Left o) = (priorityOf o, Left o)
                                         LnE -> 3
                                         Lbr -> 2
                                         Rbr -> 2
---TODO: Lookup Unsigned Integers
-
-
-detectVarsAndNumbers :: [Either Operator Token] -> [Either Operator Term]
-detectVarsAndNumbers [] = []
-detectVarsAndNumbers ((Left e):es) = (Left e) : detectVarsAndNumbers es 
-detectVarsAndNumbers ((Right e):es) = (Right (tokenToTerm e)) : (detectVarsAndNumbers es)
-
-tokenToTerm :: Token -> Term
-tokenToTerm [] = Numb 0
-tokenToTerm t@(s:ss) = if (not (elem s ['a'..'z'])) --s is a number, or atleast not a char
-                        then Numb (read t)
-                        else Var t --TODO: Catch Constants here
-
--- For testing purposes and to show general flow
-initialParse :: String -> [(Int,Either Operator Term)]
-initialParse s = map precedence (detectVarsAndNumbers (map tokenToOperator (tokenize s)))
-
-termify:: [(Int,Either Operator Term)] -> Term 
-termify [] = undefined
--- I've reached a core-term, such as a variable or a Number
-termify [(_,Right t)] = t
--- I've got Operators
-termify t = let 
-                fst@(p,fstO) = firstOperator t
-                (lhs,Left op,rhs) = splitByFirst t fst 
-                lnb@(_,lnb') = last lhs 
-                rnb@(_,rnb')= head rhs 
-                lleft = take (length lhs -1) lhs 
-                rleft = drop 1 rhs 
-            in 
-                -- I check if i reached a term
-                if (isRight fstO)
-                then termify [fst]
-                else
-                    -- I check if my neigboors are Terms?
-                    if((isRight lnb') && (isRight rnb'))
-                    then 
-                        let 
-                            lcasted = fromRight (Numb 0) lnb'
-                            rcasted = fromRight (Numb 0) rnb'
-                        in
-                            case op of 
-                                -- Unary Operator
-                                LnE -> termify (lhs ++ (15,Right (Ln rcasted)) : rleft)
-                                -- Binary Operator
-                                StarStar -> termify (lleft ++ (15,Right (Pow lcasted rcasted)) : rleft)
-                                Plus -> termify (lleft ++ (15,Right (Add lcasted rcasted)) : rleft)
-                                Minus -> termify (lleft ++ (15,Right (Sub lcasted rcasted)) : rleft)
-                                Star -> termify (lleft ++ (15,Right (Mul lcasted rcasted)) : rleft)
-                                DivSlash -> termify (lleft ++ (15,Right (Div lcasted rcasted)) : rleft)
-                                -- Any other stuff ?
-                                otherwise -> undefined
-                    else undefined
-
+                    --TODO: Lookup Unsigned Integers
+                    
 -- I Split by the Operator with the highest Priority and Return a Combination of the LefthandSide, Operator and RighthandSide
-splitByFirst :: [(Int,Either Operator Term)] -> (Int,Either Operator Term) -> ([(Int,Either Operator Term)],Either Operator Term,[(Int,Either Operator Term)])
-splitByFirst [] ops@(_,fst) = undefined
-splitByFirst t@(x:xs) ops@(i,fst) = 
-                        let (p:ps) = splitOn [ops] t
-                        in (p, fst, operatorConc ops ps)
+splitByFirst :: [Either Operator Term] -> Either Operator Term -> ([Either Operator Term],Either Operator Term,[Either Operator Term])
+splitByFirst [] _ = undefined
+splitByFirst t ops = 
+                let (p:ps) = splitOn [ops] t
+                in (p, ops, operatorConc ops ps)
 
 -- This method combines Terms i've splitted above 
 -- It's important to include the operator again
-operatorConc :: (Int,Either Operator Term) -> [[(Int,Either Operator Term)]] -> [(Int,Either Operator Term)]
+operatorConc :: Either Operator Term -> [[Either Operator Term]] -> [Either Operator Term]
 operatorConc _ [] = []
 operatorConc _ [d] = d
 operatorConc op (d:ds) = d++[op]++(operatorConc op ds)
 
-firstOperator :: [(Int,Either Operator Term)] -> (Int,Either Operator Term)
-firstOperator l@((i,t):xs) = 
-                            if i == lowest 
-                                then (i,t)
-                                else firstOperator(xs)
-                            where lowest = lowestPrio l -- TODO: Move this somewhere else, it only needs to be called once
+firstOperator :: [Either Operator Term] -> Either Operator Term
+firstOperator os =
+                let l = map precedence os
+                    lowest = lowestPrio l
+                in  getFirstOperator lowest l
+                where 
+                    getFirstOperator low ((i,t):xs) =
+                        if (i == low) 
+                            then t
+                            else getFirstOperator low (xs)
 
 -- Find me the lowest priority in my current Operator/Term-List
 lowestPrio :: [(Int,Either Operator Term)] -> Int
