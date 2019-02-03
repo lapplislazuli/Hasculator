3ee8fad9589e4008b674c4486075fe86aaf3c36a
Twonki <Leonhard.Applis@Protonmail.com>
2019-02-03 15:30:28 +0100

    Added Priority Marking to Parsing

--- Parser.hs
+++ Parser.hs
@@ -3,7 +3,8 @@
 module Parser where 
 
 import Data.Maybe (fromJust)
-import Terms 
+import Terms
+import Data.List
 
 type Token = String 
 
@@ -29,6 +30,26 @@ tokenToOperator t =
 tokenize :: String -> [Token]
 tokenize s = words s 
 
+-- Assings the priority of each Term or Operator to the Term or operator
+-- I left some out, i case i want to add more or i forgot something
+precedence :: Either Operator Term -> (Int, Either Operator Term)
+-- Finished Terms have the "lowest" Priority marked as 15
+precedence (Right t) = (15, Right t)
+-- Every Operator gets a priority
+precedence (Left o) = (priorityOf o, Left o)
+                        where priorityOf o =
+                                    case o of 
+                                        Plus -> 8
+                                        Minus -> 8
+                                        Star -> 6
+                                        DivSlash -> 6
+                                        StarStar -> 4
+                                        LnE -> 3
+                                        Lbr -> 2
+                                        Rbr -> 2
+--TODO: Lookup Unsigned Integers
+
+
 detectVarsAndNumbers :: [Either Operator Token] -> [Either Operator Term]
 detectVarsAndNumbers [] = []
 detectVarsAndNumbers ((Left e):es) = (Left e) : detectVarsAndNumbers es 
@@ -41,5 +62,24 @@ tokenToTerm t@(s:ss) = if (not (elem s ['a'..'z'])) --s is a number, or atleast 
                         else Var t --TODO: Catch Constants here
 
 -- For testing purposes and to show general flow
-initialParse :: String -> [Either Operator Term]
-initialParse s = detectVarsAndNumbers (map tokenToOperator (tokenize s))
+initialParse :: String -> [(Int,Either Operator Term)]
+initialParse s = map precedence (detectVarsAndNumbers (map tokenToOperator (tokenize s)))
+
+termify:: [Either Operator Term] -> Term 
+-- No Operators left
+termify [] = Numb 0 
+-- I've reached a core-term, such as a variable or a number
+termify [Right t] = t
+
+-- I've got an prefix unary operator
+termify ((Left fst):rest) 
+    | fst == LnE = Ln (termify rest)
+    | otherwise = undefined -- Here is also the Errorcase if i got (strangely) Operators left
+-- I've got an Infix binary operator
+termify ((Right fst):(Left snd):rest)
+    | snd == StarStar = Pow fst (termify rest)
+    | snd == DivSlash = Div fst (termify rest)
+    | snd == Star = Mul fst (termify rest)
+    | snd == Plus = Add fst (termify rest)
+    | snd == Minus = Sub fst (termify rest)
+    | otherwise = undefined -- Here are also error cases?
