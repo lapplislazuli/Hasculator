382624e48503649b8ab20a715ddb503f46098a46
Twonki <Leonhard.Applis@Protonmail.com>
2019-02-03 17:44:19 +0100

    Some Progress on Parsing

--- Parser.hs
+++ Parser.hs
@@ -3,8 +3,10 @@
 module Parser where 
 
 import Data.Maybe (fromJust)
+import Data.Either
 import Terms
 import Data.List
+import Data.List.Split (splitOn)
 
 type Token = String 
 
@@ -65,21 +67,66 @@ tokenToTerm t@(s:ss) = if (not (elem s ['a'..'z'])) --s is a number, or atleast 
 initialParse :: String -> [(Int,Either Operator Term)]
 initialParse s = map precedence (detectVarsAndNumbers (map tokenToOperator (tokenize s)))
 
-termify:: [Either Operator Term] -> Term 
--- No Operators left
-termify [] = Numb 0 
--- I've reached a core-term, such as a variable or a number
-termify [Right t] = t
+termify:: [(Int,Either Operator Term)] -> Term 
+termify [] = undefined
+-- I've reached a core-term, such as a variable or a Number
+termify [(_,Right t)] = t
+-- I've got Operators
+termify t = let 
+                fst@(p,fstO) = firstOperator t
+                (lhs,Left op,rhs) = splitByFirst t fst 
+                lnb@(_,lnb') = last lhs 
+                rnb@(_,rnb')= head rhs 
+                lleft = take (length lhs -1) lhs 
+                rleft = drop 1 rhs 
+            in 
+                -- I check if i reached a term
+                if (isRight fstO)
+                then termify [fst]
+                else
+                    -- I check if my neigboors are Terms?
+                    if((isRight lnb') && (isRight rnb'))
+                    then 
+                        let 
+                            lcasted = fromRight (Numb 0) lnb'
+                            rcasted = fromRight (Numb 0) rnb'
+                        in
+                            case op of 
+                                -- Unary Operator
+                                LnE -> termify (lhs ++ (15,Right (Ln rcasted)) : rleft)
+                                -- Binary Operator
+                                Plus -> termify (lleft ++ (15,Right (Add lcasted rcasted)) : rleft)
+                                Minus -> termify (lleft ++ (15,Right (Sub lcasted rcasted)) : rleft)
+                                Star -> termify (lleft ++ (15,Right (Mul lcasted rcasted)) : rleft)
+                                DivSlash -> termify (lleft ++ (15,Right (Div lcasted rcasted)) : rleft)
+                                -- Any other stuff ?
+                                otherwise -> undefined
+                    else undefined
 
--- I've got an prefix unary operator
-termify ((Left fst):rest) 
-    | fst == LnE = Ln (termify rest)
-    | otherwise = undefined -- Here is also the Errorcase if i got (strangely) Operators left
--- I've got an Infix binary operator
-termify ((Right fst):(Left snd):rest)
-    | snd == StarStar = Pow fst (termify rest)
-    | snd == DivSlash = Div fst (termify rest)
-    | snd == Star = Mul fst (termify rest)
-    | snd == Plus = Add fst (termify rest)
-    | snd == Minus = Sub fst (termify rest)
-    | otherwise = undefined -- Here are also error cases?
+-- I Split by the Operator with the highest Priority and Return a Combination of the LefthandSide, Operator and RighthandSide
+splitByFirst :: [(Int,Either Operator Term)] -> (Int,Either Operator Term) -> ([(Int,Either Operator Term)],Either Operator Term,[(Int,Either Operator Term)])
+splitByFirst [] ops@(_,fst) = undefined
+splitByFirst t@(x:xs) ops@(_,fst) = 
+                        let (p:ps) = splitOn [ops] t
+                        in (p, fst, localConc ps )
+                        where localConc (d:ds) = d++ (localConc ds)
+                              localConc [] = []
+
+
+firstOperator :: [(Int,Either Operator Term)] -> (Int,Either Operator Term)
+firstOperator l@((i,t):xs) = 
+                            if i == lowest 
+                                then (i,t)
+                                else firstOperator(xs)
+                            where lowest = lowestPrio l -- TODO: Move this somewhere else, it only needs to be called once
+
+-- Find me the lowest priority in my current Operator/Term-List
+lowestPrio :: [(Int,Either Operator Term)] -> Int
+lowestPrio [] = 16
+lowestPrio [(i,t)] = i
+lowestPrio ((i,t):xs) = min i (lowestPrio xs)
+
+--TODO: Why can't i load this from normal Data.Either Package?
+fromRight :: b -> Either a b -> b 
+fromRight def (Right b) = b 
+fromRight def (Left a) = def
