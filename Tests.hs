b71ae5c1b315f30f054b10c7fa0b6c723b8ffb76
Twonki <Leonhard.Applis@Protonmail.com>
2019-02-16 18:35:15 +0100

    Added Reparse Tests, closing #10 for now

--- Tests.hs
+++ Tests.hs
@@ -11,6 +11,7 @@ allTests = TestList [
     ,TestLabel "OperatorTests" operatorTests
     ,TestLabel "TokenizerTests" tokenizerTests
     ,TestLabel "Basic Show-Tests" termShowTests
+    ,TestLabel "Reparse Tests" parseShowTests
     ,TestLabel "PrecedenceTests" precedenceTests
     ,TestLabel "NegationTests" negaterTests
     ,TestLabel "VariableTests" variableTests
@@ -195,6 +196,41 @@ showErr = "TestErr" ~=? show (ErrorTerm "TestErr")
 showBinary = "(a+b)"  ~=? show (Add (Var "a") (Var "b") )
 showUnary = "Ln(b)"  ~=? show (Ln (Var "b") )
 
+parseShowTests = TestList [
+    TestLabel "Reparse Numb" repNum
+    ,TestLabel "Reparse Var" repVar
+    ,TestLabel "Reparse Const" repConst
+    
+    ,TestLabel "Reparse Addition" repAdd
+    ,TestLabel "Reparse Sub" repSub
+    ,TestLabel "Reparse Mul" repMul 
+    ,TestLabel "Reparse Div" repDiv
+    ,TestLabel "Reparse Ln" repLn 
+    ,TestLabel "Reparse Exp" repExp 
+
+    ,TestLabel "Reparse unary around binary" repUnBin
+    ,TestLabel "Reparse binary around Unary" repBinUn
+    ,TestLabel "Reparse binary around binary" repBinBin
+    ,TestLabel "Reparse unary around unary" repUnUn
+    ]
+
+repNum = (Numb 2) ~=? reparse (Numb 2)
+repVar = (Var "a") ~=? reparse (Var "a")
+repConst = (Const "e") ~=? reparse (Const "e")
+
+repAdd = (Add (Numb 2) (Var "a")) ~=? reparse (Add (Numb 2) (Var "a")) 
+repSub = (Sub (Numb 2) (Var "a")) ~=? reparse (Sub (Numb 2) (Var "a"))
+repMul = (Mul (Numb 2) (Var "a")) ~=? reparse (Mul (Numb 2) (Var "a"))
+repDiv = (Div (Numb 2) (Var "a")) ~=? reparse (Div (Numb 2) (Var "a"))
+repLn = (Ln (Var "a")) ~=? reparse (Ln (Var "a"))
+repExp = (Exp (Var "a")) ~=? reparse (Exp (Var "a"))
+
+repUnUn = (Exp (Exp (Var "a"))) ~=? reparse (Exp (Exp (Var "a"))) 
+repUnBin = (Exp (Add (Var "a")(Numb 2))) ~=? reparse (Exp (Add (Var "a")(Numb 2)))
+repBinBin = (Add (Add (Var "a") (Numb 1)) (Sub (Var "b")(Numb 1))) ~=? reparse (Add (Add (Var "a") (Numb 1)) (Sub (Var "b")(Numb 1)))
+repBinUn = (Add (Ln (Var "a")) (Exp (Var "b"))) ~=? reparse (Add (Ln (Var "a")) (Exp (Var "b")))
+
+
 solverTests = TestList [
     TestLabel "Regula Falsi I " testRegula1
     ,TestLabel "Regula Falsi II " testRegula2
@@ -309,6 +345,9 @@ parSolv str vars = solve (parse str) vars
 --Solves the Term for no-Variables (or every Variable 0)
 simParSolv :: String -> Double 
 simParSolv str = (parSolv str []) 
+
+reparse :: Term -> Term 
+reparse = parse . show
 
 truncate' :: Double -> Int -> Double
 truncate' x n = (fromIntegral (floor (x * t))) / t
